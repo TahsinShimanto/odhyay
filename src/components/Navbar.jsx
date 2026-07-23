2f3870038c0a59d6e1111633f047bd8026786b84
MD. Tahsin Haque <tahsin.tg77@gmail.com>
2026-07-23 23:29:16 +0600

    added basic properties , initialized navbar design

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import '../styles/Navbar.css'
+import { NavLink } from 'react-router-dom'
+import {HomeIcon, BookOpenCheck, Bookmark, Lock, NotebookPen, BarChart, LogIn} from 'lucide-react'
+const Navbar = () => {
+  return (
+    <div className='navbarWrapper'>
+      <div className="appLogoContainer">
+        <NavLink id='appName' to={"/"}>
+            অধ্যায়.
+        </NavLink>
+      </div>
+
+
+      <div className="pageLinks">
+        <NavLink to={"/"} className={({isActive}) => isActive? "activeLinks":"nonActiveLinks"}>
+            <HomeIcon size={18}/>
+            Home
+        </NavLink>
+
+        <NavLink to={"/"} className={({isActive}) => isActive? "activeLinks":"nonActiveLinks"}>
+            <BookOpenCheck size={18}/>
+            Question Solving
+        </NavLink>
+
+        <NavLink to={"/"} className={({isActive}) => isActive? "activeLinks":"nonActiveLinks"}>
+            <Bookmark size={18}/>
+            Saved Questions
+        </NavLink>
+
+        <NavLink to={"/"} className={({isActive}) => isActive? "activeLinks":"nonActiveLinks"}>
+            <NotebookPen size={18}/>
+            Unranked Exam
+        </NavLink>
+
+        <NavLink to={"/"} className={({isActive}) => isActive? "activeLinks":"nonActiveLinks"}>
+            <BarChart size={18}/>
+            Ranked Exam
+        </NavLink>
+      </div>
+
+        <div className="signinContainer">
+            <NavLink to={"/"} className={({isActive}) => isActive? "activeLinks":"nonActiveLinks"}>
+                <LogIn size={18}/>
+                Sign In
+            </NavLink>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
