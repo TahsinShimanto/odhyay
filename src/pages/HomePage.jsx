3a8fae9a2f218af3d1fd9690c44afc8a563ef85e
MD. Tahsin Haque <tahsin.tg77@gmail.com>
2026-07-31 20:25:09 +0600

    added some homepage designs and some responsiveness to it

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
-
+import React from "react";
+import "../styles/HomePage.css";
+import { Pen, BookOpen, ArrowBigRight, ArrowLeft, ArrowRight } from "lucide-react";
+import {NavLink} from 'react-router'
 const HomePage = () => {
   return (
-    <div>
-      home
-    </div>
-  )
-}
+    <div className="home-page-container">
+      <div className="hero-card">
+        <div className="left">
+          <div className="hero-badge">
+            <Pen size={15} />
+            Lorem ipsum dolor kit
+          </div>
 
-export default HomePage
+          <div className="hero-description">
+            <p id="hero-heading">Lorem ipsum dolor sit amet </p>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            ipsum laudantium tempora quas ad vero consectetur blanditiis ullam,
+            officiis deserunt laudantium tempora quas ad vero </span>
+          </div>
+
+          <div className="hero-page-links">
+            <NavLink
+              to={"/questionsolving"}
+              className='solve-question'
+            >
+
+              SOLVE QUESTIONS
+            </NavLink>
+
+            <NavLink
+              to={"/unrankedexam"}
+              className='custom-exam'
+            >
+              
+              CUSTOM EXAMS
+            </NavLink>
+
+            <NavLink to={"/"} className='sign-in'>
+              
+              SIGN IN TO TRACK STREAK
+              <ArrowRight size={15}/>
+            </NavLink>
+          </div>
+        </div>
+
+        <div className="right">
+          <BookOpen size={400} color="#272727" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
