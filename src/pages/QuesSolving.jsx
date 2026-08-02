7495377f301bb1af199ae25383224c6baf40650e
imran-saleh110 <imranforwork110@gmail.com>
2026-08-02 15:44:48 +0600

    Fetching questions from database(not use yet)

--- src/pages/QuesSolving.jsx
+++ src/pages/QuesSolving.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const QuesSolving = () => {
+  const [questions ,setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      
+    }
+  })
+
   return (
     <div>
       QuesSolving
