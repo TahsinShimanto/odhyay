f5d2f98f731839623e76e291bf82651eb4e64460
imran-saleh110 <imranforwork110@gmail.com>
2026-08-07 21:04:31 +0600

    added style to the written questions

--- src/components/WrittenQuestion.jsx
+++ src/components/WrittenQuestion.jsx
@@ -1,35 +1,57 @@
-import React from 'react';
-import { Bookmark, ShieldAlert, ChevronLeft, ChevronRight, CheckCircle2, XCircle, Search, HelpCircle, Eye, EyeOff, AlertTriangle, Star } from 'lucide-react'
+import { useState } from 'react';
+import { Bookmark, ShieldAlert, Eye, Star } from 'lucide-react'
 import '../styles/WrittenQuestion.css'
 
 function Written({question, current, total}) {
+    const [showAnswer, setShowAnswer] = useState(false);
+
     return (
         <div className="question-card">
-            <div className="queston-card-info">
+            <div className="question-card-info">
                 <div className="question-info-count">প্রশ্ন {current}/{total}</div>
-                <div className="question-info-importance">{question.importance}</div>
-                <div className="question-info-taglike">লিখিত প্রশ্ন</div>
+                <div className="question-info-importance">
+                    {Array.from({ length: question.importance }).map((_, index) => (
+                        <Star
+                            key={index}
+                            size={14}
+                            className="star"
+                        />
+                    ))}
+                </div>
+                <div className="question-info-type">নির্বাচনী প্রশ্ন</div>
                 <button className="question-info-save"><Bookmark className="save-fill" size={16}/></button>
                 <button className="question-info-flag"><ShieldAlert className="flag-fill" size={16}/></button>
             </div>
 
-            <div className="question-statement-container">
-                <span className="question-statement">{question.questionText}</span>
-                <span className="question-image">{question.questionimage}</span>
-            </div>
+            <div className="question-card-main">
+                <div className="question-statement-container">
+                    <span className="question-statement">{question.questionText}</span>
+                    <span className="question-image">{question.questionimage}</span>
+                </div>
 
-            <div className="question-occurrences">
-                {question.appearances.map((object) =>
-                    <div key={object.id} className="appearance-tag">
-                        <span className="bullet"></span> {object.university} {object.year}
-                    </div>
-                )}
-            </div>
+                <div className="question-occurrences">
+                    {question.appearances.map((object) =>
+                        <div key={object.id} className="appearance-tag">
+                            <span className="bullet"></span> {object.university} {object.year}
+                        </div>
+                    )}
+                </div>
 
-            <div className="divider"></div>
-            <div className="explaination-card">
-                {question.answerText}
-                {question.answerImage}
+                <div className={(!showAnswer) ? "hidden-answer-card" : "revealed-answer-card"} onClick={() => setShowAnswer(true)}>
+                    {showAnswer ? (
+                        <>
+                            <div className="hide-answer-button" onClick={(e) => { e.stopPropagation(); setShowAnswer(false)}}>উত্তর লুকান</div>
+                            <div>{question.answerText}</div>
+                            <div>{question.answerImage}</div>
+                        </>
+
+                    ) : (
+                        <>
+                            <Eye size={16}/>
+                            <div>উত্তর দেখুন</div>
+                        </>
+                    )}
+                </div>
             </div>
         </div>
     );
