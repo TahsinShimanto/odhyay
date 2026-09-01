4ec49218163c2753e9308216bdc74a7cc3ef4317
imran-saleh110 <imranforwork110@gmail.com>
2026-09-02 02:27:20 +0600

    fixed the port definition bug

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,9 @@ import dns from 'dns'
 
 dns.setServers(['8.8.8.8', '1.1.1.1']);
 
+// Dotenv
+dotenv.config()
+
 // App config
 const app = express()
 const port = process.env.PORT || 4000
@@ -20,12 +23,8 @@ app.listen(port, () => {
   console.log('server started on port: ' + port);
 })
 
-// Dotenv
-dotenv.config()
-
 // Connect to MongoDB
 connectDB()
-
 
 ///////////////////API Endpoints///////////////////////
 
